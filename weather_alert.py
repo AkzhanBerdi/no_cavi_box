e676e0876da8aeeef18cae955592d1575419cc57
AkzhanBerdi <113842172+AkzhanBerdi@users.noreply.github.com>
2022-09-23 00:49:43 +0600

    Update weather_alert.py

--- weather_alert.py
+++ weather_alert.py
@@ -1,4 +1,3 @@
-#tools
 from ctypes.wintypes import MSG
 from bs4 import BeautifulSoup
 import os
@@ -6,7 +5,6 @@ import requests
 import smtplib
 import time
 import datetime
-#import pyautogui
 import telebot
 
 token = "5739207168:AAFNcX2_M_oJVQs-RcEaLGLd3L1dMuAPW8s"
@@ -21,9 +19,8 @@ soup1 = BeautifulSoup(page.content,"html.parser")
 soup2 = BeautifulSoup(soup1.prettify(),"html.parser")
 weather = soup2.find(id='header').get_text()
 weather = " ".join(weather.split())[106:]
-hours = 0
 
-#for_if
+#weather conditions to be further advised
 w1 = "Ясно"
 w2 = "Переменная облачность"
 w3 = "Снег"
@@ -32,12 +29,11 @@ w41 = "Облачно, с дождем"
 w5 = "Переменная облачность с кратковременными"
 w6 = "Пасмурно"
 
-#date
+#date modules
 import datetime
 today = datetime.date.today()
-tik_tok = datetime.time()
 
-#Create CSV
+#Create CSV command
 import csv
 header = ['weather','date','time']
 data = [weather,today,tik_tok]
@@ -46,80 +42,78 @@ with open('powderbot.csv', 'w', newline='', encoding='UTF8') as f:
     writer.writerow(header)
     writer.writerow(data)
 
-#Read CSV
-#import pandas as pd
-#df = pd.read_csv(r'C:\Users\Admin\powderbot.csv')
-
-#CSV append
+#CSV append command
 with open('powderbot.csv', 'a+', newline='', encoding='UTF8') as f:
     writer = csv.writer(f)
     writer.writerow(data)
 
-#Hourly report
+#Hourly report function
 def alert(): 
     URL = "https://www.meteoblue.com/ru/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0/%D1%81%D0%B5%D0%B3%D0%BE%D0%B4%D0%BD%D1%8F/shymbulak-mountain-resort_%d0%9a%d0%b0%d0%b7%d0%b0%d1%85%d1%81%d1%82%d0%b0%d0%bd_11496678"
-
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
-
     page = requests.get(URL,headers=headers)
-
     soup1 = BeautifulSoup(page.content,"html.parser")
-
     soup2 = BeautifulSoup(soup1.prettify(),"html.parser")
-
     weather = soup2.find(id='header').get_text()
-
     weather = " ".join(weather.split())[106:]
 
-    hours = 0
-
     import datetime
-
     today = datetime.date.today()
 
-    tik_tok = datetime.time()
-
     import csv
-
-    header = ['weather','date','time']
-    data = [weather,today,tik_tok]
+    header = ['weather','date']
+    data = [weather,today]
 
     with open('powderbot.csv', 'a+', newline='', encoding='UTF8') as f:
         writer = csv.writer(f)
         writer.writerow(data)
-    #pyautogui.hotkey('f5')
-    #print(df)
     
     if w4 in weather:
         bot.send_message(chat_id=-1001693361742,text="Привет люди! На чиме идёт дождь, а значит зонтик не помешает...")
+        start = time.time()
         while(True):
             continue
+        finish = time.time()
+        total_time = finish - start
+        bot.send_message(chat_id=-1001693361742,text="Дождь закончился, продолжительность - ", total_time)
     elif w41 in weather:
-            bot.send_message(chat_id=-1001693361742,text="На чиме облачно с дождем, одевайтесь теплее...")
-            while(True):
-                continue
+        bot.send_message(chat_id=-1001693361742,text="На чиме облачно с дождем, одевайтесь теплее...")
+        start = time.time()
+        while(True):
+            continue
+        finish = time.time()
+        total_time = finish - start
+        bot.send_message(chat_id=-1001693361742,text="Дождь закончился, продолжительность - ", total_time)
     elif w3 in weather:
-            bot.send_message(chat_id=-1001693361742,text="Ура! На чиме идёт снег!")
-            while(True):
-                continue
+        bot.send_message(chat_id=-1001693361742,text="Ура! На чиме идёт снег!")
+        start = time.time()
+        while(True):
+            continue
+        finish = time.time()
+        total_time = finish - start
+        bot.send_message(chat_id=-1001693361742,text="Снег закончился, продолжительность - ", total_time)
     elif w5 in weather:
-            bot.send_message(chat_id=-1001693361742,text="На чиме кратковременные дожди, возьмите зонтик!")
-            while(True):
-                continue
+        bot.send_message(chat_id=-1001693361742,text="На чиме кратковременные дожди, возьмите зонтик!")
+        start = time.time()
+        while(True):
+            continue
+        finish = time.time()
+        total_time = finish - start
+        bot.send_message(chat_id=-1001693361742,text="Дождь закончился, продолжительность - ", total_time)
     elif w1 in weather:
-            bot.send_message(chat_id=-1001693361742,text="На чиме ясно")
-            while(True):
-                continue
+        bot.send_message(chat_id=-1001693361742,text="На чиме прояснилось")
+        while(True):
+            continue
     elif w2 in weather:
-            bot.send_message(chat_id=-1001693361742,text="На чиме облачно")
-            while(True):
-                continue
+        bot.send_message(chat_id=-1001693361742,text="На чиме облачно")
+        while(True):
+            continue
     elif w1 in weather:
-            bot.send_message(chat_id=-1001693361742,text="На чиме пасмурно")
-            while(True):
-                continue
+        bot.send_message(chat_id=-1001693361742,text="На чиме пасмурно")
+        while(True):
+            continue
     else:
-        bot.send_message(chat_id=-1001693361742,text="Обнаружена неизвестная погода")
+        bot.send_message(chat_id=-1001693361742,text="Обнаружена неопознанная погода, добавьте в базу данных")
 while(True):
     alert()
-    time.sleep(300)
+    time.sleep(600)
