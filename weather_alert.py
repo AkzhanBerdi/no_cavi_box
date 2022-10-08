a20e099129c06db800add0d036d657733da0401d
AkzhanBerdi <113842172+AkzhanBerdi@users.noreply.github.com>
2022-10-08 20:40:34 +0600

    Update weather_alert.py

--- weather_alert.py
+++ weather_alert.py
@@ -1,3 +1,4 @@
+#IMPORTS
 from ctypes.wintypes import MSG
 from bs4 import BeautifulSoup
 import os
@@ -6,20 +7,12 @@ import smtplib
 import time
 import datetime
 import telebot
-
+import datetime
+import csv
+#VARS
 token = "5739207168:AAFNcX2_M_oJVQs-RcEaLGLd3L1dMuAPW8s"
 bot = telebot.TeleBot(token)
 bot.config['api_key'] = token
-
-#web_scrap
-URL = "https://www.meteoblue.com/ru/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0/%D1%81%D0%B5%D0%B3%D0%BE%D0%B4%D0%BD%D1%8F/shymbulak-mountain-resort_%d0%9a%d0%b0%d0%b7%d0%b0%d1%85%d1%81%d1%82%d0%b0%d0%bd_11496678"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
-page = requests.get(URL,headers=headers)
-soup1 = BeautifulSoup(page.content,"html.parser")
-soup2 = BeautifulSoup(soup1.prettify(),"html.parser")
-weather = soup2.find(id='header').get_text()
-weather = " ".join(weather.split())[106:]
-
 URL = "https://www.meteoblue.com/ru/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0/%D1%81%D0%B5%D0%B3%D0%BE%D0%B4%D0%BD%D1%8F/shymbulak-mountain-resort_%d0%9a%d0%b0%d0%b7%d0%b0%d1%85%d1%81%d1%82%d0%b0%d0%bd_11496678"
 CHATBOT_ID = -1001693361742
 TEXT_STATUSES = {
@@ -32,23 +25,26 @@ TEXT_STATUSES = {
     "Пасмурно": "Тамп пасмурно"
 }
 
+#SCRAP
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
+page = requests.get(URL,headers=headers)
+soup1 = BeautifulSoup(page.content,"html.parser")
+soup2 = BeautifulSoup(soup1.prettify(),"html.parser")
+weather = soup2.find(id='header').get_text()
+weather = " ".join(weather.split())[106:]
+today = datetime.date.today()
+header = ['weather','date']
+data = [weather,today]
 #date modules
-#import datetime
-#today = datetime.date.today()
+
 
 #Create CSV command
-#import csv
-#header = ['weather','date']
-#data = [weather,today]
+
+
 with open('powderbot.csv', 'w', newline='', encoding='UTF8') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     writer.writerow(data)
-
-#CSV append command
-#with open('powderbot.csv', 'a+', newline='', encoding='UTF8') as f:
- #   writer = csv.writer(f)
-  #  writer.writerow(data)
 
 #Hourly report function
 def alert(chat_id, text_statuses, past_status, url, past_time): 
@@ -59,27 +55,21 @@ def alert(chat_id, text_statuses, past_status, url, past_time):
     weather = soup2.find(id='header').get_text()
     weather = " ".join(weather.split())[106:]
 
-    import datetime
-    today = datetime.date.today()
-
-    import csv
-    header = ['weather','date']
-    data = [weather,today]
-
     with open('powderbot.csv', 'a+', newline='', encoding='UTF8') as f:
         writer = csv.writer(f)
         writer.writerow(data)
     
     for w_status, text in text_statuses.items():
         if w_status in weather:
+            hours += 1
             if w_status != past_status:
+                hours = 0
                 bot.send_message(chat_id=chat_id, text=text)
                 
-    return w_status, current_time
+    return past_weather_status, past_time
     
-            
+"""
 def hours(chat_id, weather_status):
-    hours = 0
     if w5 in weather:
         hours += 1
         bot.send_message(chat_id=chat_id,text=f"дождь продолжается, уже льёт {hours} час")
@@ -90,14 +80,15 @@ def hours(chat_id, weather_status):
             else:
                 bot.send_message(chat_id=chat_id,text=f"дождь закончился, продолжался {hours} часов")
                 
-
+"""
 
 
 while(True):
+    time.sleep(60)
     past_weather_status, past_time = alert(
         chat_id=CHATBOT_ID, 
         text_statuses=TEXT_STATUSES, 
         past_status=past_weather_status,
         url=URL,
-        past_time=past_time)
-    time.sleep(60)
+        past_time=past_time
+    )
